5f6e6a58fe6e9617b1f0d668902fcac6c29c0c73
omer_dogan <omer@4alabs.io>
2022-06-18 19:10:31 +0300

    api endpoint added

---
+++ todolist_api/src/Repository/TodoListRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TodoList;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<TodoList>
+ *
+ * @method TodoList|null find($id, $lockMode = null, $lockVersion = null)
+ * @method TodoList|null findOneBy(array $criteria, array $orderBy = null)
+ * @method TodoList[]    findAll()
+ * @method TodoList[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TodoListRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TodoList::class);
+    }
+
+    public function add(TodoList $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(TodoList $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function update(TodoList $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
